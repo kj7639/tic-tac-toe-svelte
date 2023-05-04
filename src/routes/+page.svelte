66e94cb3be981bfbe9aecf13da86c7e47e7a013c
Kiera Johnson <16164505@edu.cbe.ab.ca>
2023-05-04 10:23:25 -0600

    working on making winning officially possible

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,58 @@
 <script>
     let cells = [0,1,2,3,4,5,6,7,8];
+    let winningCombos = [[0,1,2], [3,4,5], [6,7,8,], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
     let turn = false;
     let playerOneColour = "#7a2048";
     let playerTwoColour = "#1e2761"
     let cellColours = ["white", "white", "white", "white", "white", "white", "white", "white", "white"];
-    
+    let playerOneCells = [];
+    let playerTwoCells = [];
+
     function change(cell){
-        if(turn == false){
-            cellColours[cell] = playerOneColour;
-            turn = true;
-        }else{
-            cellColours[cell] = playerTwoColour;
-            turn = false;
+        if(cellColours[cell] == "white"){
+            if(turn == false){
+                cellColours[cell] = playerOneColour;
+                playerOneCells.push(cell);
+                console.log(playerOneCells);
+                turn = true;
+            }else{
+                cellColours[cell] = playerTwoColour;
+                playerTwoCells.push(cell)
+                console.log(playerTwoCells);
+                turn = false;
+            }
         }
     }
     function clear(){
         cellColours = ["white", "white", "white", "white", "white", "white", "white", "white", "white"];
     }
+    $: {
+        let winPoints = 0;
+        for(let i = 0; i < 3; i++){
+            if(playerOneCells.includes(winningCombos[0][i])){
+                winPoints ++;
+                console.log("win points:" + winPoints);
+            }
+            if(winPoints == 3){
+                console.log("you won!");
+            }
+        }
+        winPoints = 0;
+    }
 </script>
 
 <title>Tic Tac Toe</title>
 <h1>Tic Tac Toe</h1>
+{#if turn == false}
+    <h2 style="color:{playerOneColour};">Player One's Turn</h2>
+{:else}
+    <h2 style="color:{playerTwoColour};">Player Two's Turn</h2>
+{/if}
 <button on:click={clear}>Clear</button>
 <div class="board">
     {#each cells as cell}
         <div id={cell} on:click={() => change(cell)} style="background-color:{cellColours[cell]}"></div>
     {/each}
-    <!-- <div id="1" on:click={() => change(1)} style="background-color:{playerOneColor};"></div>
-    <div id="2" on:click={() => change(2)}></div>
-    <div id="3" on:click={() => change(3)}></div>
-    <div id="4" on:click={() => change(4)}></div>
-    <div id="5" on:click={() => change(5)}></div>
-    <div id="6" on:click={() => change(6)}></div>
-    <div id="7" on:click={() => change(7)}></div>
-    <div id="8" on:click={() => change(8)}></div>
-    <div id="9" on:click={() => change(9)}></div> -->
 </div>
 
 <style>
